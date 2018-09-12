d1bc86abe5cf24a64639fe2458f3732b1e01fcc1
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-09-12 18:21:15 +0100

    grid done, need to do click colours

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', function(){
   // Add code here
   const board = document.getElementById('board');
 
+  const numOfRows = Number(prompt('How many rows would you like?'));
+  const numOfColumns = Number(prompt('How many columns would you like?'));
 
+  for (i=0; i<numOfRows; i++) {
+    const rows = document.createElement('div');
+    rows.setAttribute('class','row'); // first argument: what type do you want (e.g. id, class etc.), second argument: the class or id you actually want to assign it to, so in this case we assign it to the row class (see css)
+    board.appendChild(rows); // append the argument (rows) to board
+    for (a=0; a<numOfColumns; a++) {
+      const columns = document.createElement('div');
+      columns.setAttribute('class','col');
+      board.appendChild(columns);
+    }
+  }
 
 });
