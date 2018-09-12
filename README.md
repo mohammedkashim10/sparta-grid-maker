90e37276b782e3fcaf088bf322132b6b47f11231
Tech-A36 <tech-a36@tech-a36s-air.home>
2018-09-12 23:21:06 +0100

    grid with colours done

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sparta-grid-maker
+### Description
+The task was to generate a grid with number of rows and columns determined by an input using prompts. Thereafter, when a square inside the grid is clicked, it changes colour to a random colour.
+Bonus: add sound when a square is clicked
+
+### Languages Used
+* JavaScript
+
+### How to Download
+1. Link to the GitHub repository: https://github.com/mohammedkashim10/sparta-grid-maker
+2. Click on 'Clone or download' and copy the SSH key (git@github.com:mohammedkashim10/sparta-grid-maker.git)
+3. In your terminal, in your desired location, carry out the command 'git clone -SSH key-'
+
+### Challenges
+I found it difficult to get started with the generating of the grid because I didn't really know how to go about doing it. Being inexperienced with DOM, I found the notations and invocations tricky, for example *element*.setAttributes(). It took me a while to get used to and understand what exactly is happening with such commands.
+
+### Learning Points
+I'm slowly starting to understand what methods and events actually are, and what they do when carried out. Although still not fully comfortable, conceptually my understanding of certain methods is improving.
--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
-  // Add code here
+
   const board = document.getElementById('board');
 
-  const numOfRows = Number(prompt('How many rows would you like?'));
+  const numOfRows = Number(prompt('How many rows would you like?')); // 'Number' saves the prompt as a number
   const numOfColumns = Number(prompt('How many columns would you like?'));
+
+  function randomColor() { // this function generates a random colour
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (i=0; i<6; i++) {
+      color += letters[Math.floor(Math.random()*16)];
+    }
+    return color;
+  }
 
   for (i=0; i<numOfRows; i++) {
     const rows = document.createElement('div');
-    rows.setAttribute('class','row'); // first argument: what type do you want (e.g. id, class etc.), second argument: the class or id you actually want to assign it to, so in this case we assign it to the row class (see css)
+    rows.setAttribute('class','row'); // first argument: what type do you want to set it (e.g. id, class etc.), second argument: the class or id you actually want to assign it to, so in this case we assign it to the row class (see css)
     board.appendChild(rows); // append the argument (rows) to board
     for (a=0; a<numOfColumns; a++) {
       const columns = document.createElement('div');
       columns.setAttribute('class','col');
       board.appendChild(columns);
+      columns.addEventListener('click', () => { // assigning a click event to the column components
+        columns.style.backgroundColor = randomColor(); // what happens when clicked i.e. in this case, the background colour changes to a random colour (due to invoking the randomColor() function)
+      })
     }
   }
+
 
 });
